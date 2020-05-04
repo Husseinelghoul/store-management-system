b3eb714c5a899d3ce466c511a9d29f3fcf46c264
Ali ElZein <alielzein753@gmail.com>
2020-05-04 06:37:37 +0300

    db credentials in config.json

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
+config = require('./config.json');
+
 app.set('view engine', 'pug');
 
-var connection = mysql.createConnection({
-    host: "eu-cdbr-west-03.cleardb.net",
-    user: "babfd5078a28a6",
-    password: "8623996e",
-    database : "heroku_15ccde519ed0807"
-});
+// BETTER THAN HARDCODING
+var connection = mysql.createConnection(config['dbstring']);
   
 app.get('/', function(req, res){
     connection.query('SELECT * FROM PRODUCT', function (err, products) {
         if (err) throw err;
-        res.render('index', {products: products});
+        res.render('products', {products: products});
     });
 });
+
+app.get('/product/:barcode', function(req, res){
+    // SELECT * FROM PRODUCT WHERE barcode = ${req.params['barcode']}
+    res.render('product', {barcode: req.params['barcode']});
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
