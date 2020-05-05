33ed9d5dce7eaaf87cb6900ae8c4c22b189ec0c4
Ali ElZein <alielzein753@gmail.com>
2020-05-06 00:19:49 +0300

    process.env.PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express    = require('express');
 const mysql      = require('mysql');
 const bodyParser = require('body-parser');
-const port       = 3000;
+const port       = process.env.PORT || 3000;
 const app        = express();
 
 var connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
